227ab19d09913c292d668e405248695233ffc1f3
yewwee <chuayewwee@gmail.com>
2023-12-16 22:05:47 -0800

    update image size

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -34,7 +34,7 @@ const Product = () => {
           </div>
           <div className={`w-full sm:w-1/2 p-6`}>
             <img
-              className="max-h-96"
+              className="max-h-128"
               src={firstItem?.img}
               alt={firstItem?.title}
             />
@@ -43,7 +43,7 @@ const Product = () => {
         <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
           <div className={`w-full sm:w-1/2 p-6`}>
             <img
-              className="max-h-96"
+              className="max-h-128"
               src={secondItem?.img}
               alt={secondItem?.title}
             />
@@ -70,7 +70,7 @@ const Product = () => {
           </div>
           <div className={`w-full sm:w-1/2 p-6`}>
             <img
-              className="max-h-96"
+              className="max-h-128"
               src={thirdItem?.img}
               alt={thirdItem?.title}
             />
