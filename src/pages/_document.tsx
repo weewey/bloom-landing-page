b487e3aa3cd8f5caba1cc7d8e53a856e745541e9
yewwee <chuayewwee@gmail.com>
2023-12-16 21:52:40 -0800

    set favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <title>Bloom AI</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
